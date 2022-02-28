c9020aa8732be8af118e83ace51d4506e7885022
Adam Matteson <matteson211@hotmail.com>
2022-02-28 22:22:13 +0800

    did styling and html for settings bar

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 const rows = 8;
 const columns = 8;
 
-const gridContainer = document.querySelector('#grid-container');
-  console.log (gridContainer);
+
 
 // const square = document.createElement('div');
 //   console.log(square)
-makeSquare(5, 8)
+makeSquare(15, 15)
 
 
 
 function makeSquare(x, y) {
+
+  const gridContainer = document.querySelector('#grid-container');
+  console.log(gridContainer);
+  // gridContainer.classList.add('grid-container')
 
   for (let i = 0; i < x * y; i++) {
     let square = document.createElement('div');
     // console.log(square)
     square.classList.add('square')
     // square.textContent = 'click to change color'
-    square.addEventListener('mouseenter', (e) => {
-      e.target.classList.toggle('active')
+    square.addEventListener('mouseover', (e) => {
+      e.target.classList.add('black')
     });
     // square.classList.add('className')
     gridContainer.appendChild(square);
