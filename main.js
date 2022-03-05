5513e6a7d7131b24d8604b3049aaddd70d31e286
Adam Matteson <matteson211@hotmail.com>
2022-03-05 18:47:06 +0800

    embedded event listeners were slowing down functionality. Works now with three colors. Add resizing and color choice

--- main.js
+++ main.js
@@ -1,31 +1,86 @@
-const rows = 8;
-const columns = 8;
 
-
-
-// const square = document.createElement('div');
-//   console.log(square)
-makeSquare(15, 15)
-
-
+makeSquare(30, 20);
+// (columns, rows)
 
 function makeSquare(x, y) {
-
   const gridContainer = document.querySelector('#grid-container');
   console.log(gridContainer);
   // gridContainer.classList.add('grid-container')
-
   for (let i = 0; i < x * y; i++) {
     let square = document.createElement('div');
-    // console.log(square)
     square.classList.add('square')
     // square.textContent = 'click to change color'
-    square.addEventListener('mouseover', (e) => {
-      e.target.classList.add('black')
-    });
-    // square.classList.add('className')
     gridContainer.appendChild(square);
-    // console.log(gridContainer)
-  }
 
-}
+    const radioButton = document.querySelectorAll('.form-check-input')
+    radioButton.forEach((button) => {
+      button.addEventListener('click', () => {
+        if (document.getElementById('choose-color').checked) {
+          console.log("choose color")
+        }
+        else if (document.getElementById('red-pen').checked) {
+          // console.log("red")
+          // redPen();
+          square.addEventListener('mouseover', (e) => {
+            e.currentTarget.classList.remove('blue')
+            e.currentTarget.classList.remove('black')
+            e.currentTarget.classList.add('red')
+          })
+        }
+        else if (document.getElementById('black-pen').checked) {
+          // console.log("black")
+          // blackPen();
+          square.addEventListener('mouseover', (e) => {
+            e.currentTarget.classList.remove('blue')
+            e.currentTarget.classList.remove('red')
+            e.currentTarget.classList.add('black')
+          })
+        }
+        else if (document.getElementById('blue-pen').checked) {
+          // console.log("blue")
+          // bluePen();
+          square.addEventListener('mouseover', (e) => {
+            e.currentTarget.classList.remove('red')
+            e.currentTarget.classList.remove('black')
+            e.currentTarget.classList.add('blue')
+          })
+        }
+        else {
+          console.log('nada')
+        }
+      })
+    })
+  };
+};
+
+// function blackPen() {
+//   const divs = document.querySelectorAll('.square')
+//   // console.log(divs);
+//   divs.forEach ((div) => {
+
+//     div.addEventListener('mouseover', (e) => {
+//       e.currentTarget.classList.add('black')
+//     })
+
+//   })
+// };
+
+// function redPen() {
+//   const divs = document.querySelectorAll('.square')
+//   // console.log(divs);
+//   divs.forEach((div) => {
+//     div.addEventListener('mouseover', (e) => {
+//       e.currentTarget.classList.add('red')
+//     })
+//   })
+// };
+
+// function bluePen() {
+//   const divs = document.querySelectorAll('.square')
+//   // console.log(divs);
+//   divs.forEach((div) => {
+//     div.addEventListener('mouseover', (e) => {
+//       e.currentTarget.classList.add('blue')
+//     })
+//   })
+// };
